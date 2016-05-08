8df2b09f83dd37a969a6e811dd88e89835127abd
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2016-05-08 14:07:24 +0100

    Add tv monitor

---
+++ tv_monitor.py
@@ -0,0 +1,10 @@
+import cec_power
+import remote_command
+from time import sleep
+
+while True:
+    new_state = cec_power.power_status(0)
+    if new_state - old_state == 1:
+        remote_command.remote_com("TV_AUDIO")
+    old_state = cec_power.power_status(0)
+    sleep(1)
