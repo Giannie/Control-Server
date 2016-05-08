0f3342b930799301bf0ebe52e1c78a6a40ac43c9
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2016-05-08 14:09:22 +0100

    Initialize old state

--- tv_monitor.py
+++ tv_monitor.py
@@ -2,6 +2,8 @@ import cec_power
 import remote_command
 from time import sleep
 
+old_state = cec_power.power_status(0)
+
 while True:
     new_state = cec_power.power_status(0)
     if new_state - old_state == 1:
